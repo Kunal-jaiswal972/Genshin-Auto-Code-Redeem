49bf265d4469f340a0fe2c6b97bc90deb958bbfb
Kunal-jaiswal972 <kishujaiswal3@gmail.com>
2026-06-08 14:45:47 +0530

    frontend gone

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,6 @@ import cors from "cors";
 import morgan from "morgan";
 import express from "express";
 import { config } from "dotenv";
-import path from "path";
 import router from "./routes/router.js";
 
 config();
@@ -16,13 +15,5 @@ app.use(morgan("dev"));
 app.disable("x-powered-by");
 
 app.use("/api/v1", router);
-
-const __dirname = path.resolve();
-const frontendPath = path.join(__dirname, "./frontend/dist");
-app.use(express.static(frontendPath));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(frontendPath, "index.html"));
-});
 
 export default app;
